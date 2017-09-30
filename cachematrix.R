574935bd71d96cfa2ab785d930788370f2df56c6
Rossthesheep <Rossthesheep@users.noreply.github.com>
2017-09-30 09:54:42 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,23 @@
-makeCacheMatrix <- function(x = matrix()) {
-  
+## This function does the following: 1. Set the value of x. 
+makeCacheMatrix <- function(x = matrix()) {  
   y <- NULL
   set <- function(m) {
     x <<- m
     y <<- NULL
   }
+  ##Gets the value of x
   get <- function() x
+  ##Sets the inverse of x (y)
   setsolve <- function(solve) y <<- solve
+  ##Gets the value of y.
   getsolve <- function() y
   list(set = set,
        get = get,
        setsolve = setsolve,
        getmean = getmean)
 }
-
-cacheSolve <- function(x){
-  ## Return a matrix that is the inverse of 'x'
-  cacheSolve <- function(x) {
+##Checks if matrix have been inverted if not inverts the matrix
+cacheSolve <- function(x) {
     ## Return a matrix that is the inverse of 'x'
     y <- getsolve()
     if (!is.null(y)) {
@@ -26,9 +27,7 @@ cacheSolve <- function(x){
     data <- get()
     y <- solve(data)
     setsolve(y)
-    y
   }
-}
   
 #Makes a randomly generated 3x3 matrix and inverts it with solve
 set.seed(42)
